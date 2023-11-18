3ae8b3c77cc4300b21d92eadf2d76ae16e257347
Alexandre Moreira <4lexandre.moreira@gmail.com>
2023-11-18 13:06:17 -0300

    feat: aula 02 - Criando estilo do card-news

--- 1-fundamentos/src/Components/CardNews.js
+++ 1-fundamentos/src/Components/CardNews.js
@@ -19,6 +19,7 @@ class CardNews extends HTMLElement{
 
         const linkTitle = document.createElement("a");
         linkTitle.textContent = this.getAttribute("title");
+        linkTitle.href = this.getAttribute("link-url");
 
         const newsContent = document.createElement("p");
         newsContent.textContent = this.getAttribute("content");
@@ -30,7 +31,10 @@ class CardNews extends HTMLElement{
 
         const cardRight = document.createElement("div");
         cardRight.setAttribute("class", "card__right");
+
         const newsImage = document.createElement("img");
+        newsImage.src = this.getAttribute("photo") || "./assets/foto-default.jpeg";
+        newsImage.alt = "Foto da Noticia"
         cardRight.appendChild(newsImage);
 
         componentRoot.appendChild(cardLeft);
@@ -39,7 +43,49 @@ class CardNews extends HTMLElement{
         return componentRoot
     }
 
-    styles(){}
+    styles(){
+        const style = document.createElement("style");
+        style.textContent = `
+        .card{
+            width: 80%;
+            border: 1px solid gray;
+            display: flex;
+            flex-direction: row;
+        }
+        
+        .card{
+            box-shadow: 9px 9px 27px 0px rgba(0,0,0,0.75);
+            -webkit-box-shadow: 9px 9px 27px 0px rgba(0,0,0,0.75);
+            -moz-box-shadow: 9px 9px 27px 0px rgba(0,0,0,0.75);
+            justify-content: space-between;
+        }
+        
+        .card__left{
+            display: flex;
+            flex-direction: column;
+            justify-content: center;
+            padding: 10px;
+        }
+        
+        .card__left > a{
+            margin-top: 15px;
+            font-size: 24px;
+            text-decoration: none;
+            font-weight: bold;
+            color: black;
+        }
+        
+        .card__left > span{
+            font-weight: 700;
+        }
+        
+        img{
+            width: 350px;
+            height: 200px;
+        }
+        `
+        return style;
+    }
 }
 
 customElements.define("card-news", CardNews);
