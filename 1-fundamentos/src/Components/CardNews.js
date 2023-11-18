94a44cc35623dfcb3d1b294c00f5950b3fb1fd11
Alexandre Moreira <4lexandre.moreira@gmail.com>
2023-11-18 11:09:28 -0300

    feat: aula 02 - Construindo  props card-news

--- 1-fundamentos/src/Components/CardNews.js
+++ 1-fundamentos/src/Components/CardNews.js
@@ -3,8 +3,43 @@ class CardNews extends HTMLElement{
         super();
 
         const shadow = this.attachShadow({mode: "open"});
-        shadow.innerHTML = "<h1>Hello Alexandre!</h1>";
+        shadow.appendChild(this.build());
+        shadow.appendChild(this.styles());
     }
+
+    build(){
+        const componentRoot = document.createElement("div");
+        componentRoot.setAttribute("class", "card")
+
+        const cardLeft = document.createElement("div");
+        cardLeft.setAttribute("class", "card__left")
+
+        const autor = document.createElement("span");
+        autor.textContent = "By " + (this.getAttribute("autor") || "Anonimous");
+
+        const linkTitle = document.createElement("a");
+        linkTitle.textContent = this.getAttribute("title");
+
+        const newsContent = document.createElement("p");
+        newsContent.textContent = this.getAttribute("content");
+
+        cardLeft.appendChild(autor);
+        cardLeft.appendChild(linkTitle);
+        cardLeft.appendChild(newsContent);
+
+
+        const cardRight = document.createElement("div");
+        cardRight.setAttribute("class", "card__right");
+        const newsImage = document.createElement("img");
+        cardRight.appendChild(newsImage);
+
+        componentRoot.appendChild(cardLeft);
+        componentRoot.appendChild(cardRight);
+
+        return componentRoot
+    }
+
+    styles(){}
 }
 
 customElements.define("card-news", CardNews);
